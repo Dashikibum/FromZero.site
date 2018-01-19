c6521f269871592e53b24cb6ff9e57e1a5e0ddbc
dashikibum <syrekorazy@gmail.com>
2018-01-19 06:39:23 +0000

    Comment

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="создание web сайта с нуля, база данных mysql, php, js,jQuery, wordpress, bootstrap">
     <link href="style.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:700" rel="stylesheet"> 
     <link href="https://fonts.googleapis.com/css?family=Merriweather|Open+Sans:600" rel="stylesheet">  
@@ -22,5 +23,14 @@
             </div>
           </div>
         </header><!--header-->
+        
+            <aside>
+              <form name = "search" class="search-form" role="search" method="get">
+                <label>
+                  <input class="placehold" placeholder="Поиск..." value="" name = "searching" type="search">
+                </label>
+                <input value="Поиск" type="submit">
+              </form>
+              
         <div class="content">
           <div class="podcontent">
